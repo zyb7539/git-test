3e8ef46b79515f896fea397a35df20685bac153e
zyb <zyb.com>
2023-12-23 16:53:54 +0800

    push commit

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("Hello git3");
         System.out.println("Hello git4");
         System.out.println("master test");
+        System.out.println("push test");
 
     }
 }
