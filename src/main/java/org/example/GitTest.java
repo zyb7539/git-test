3d0c19e8fd3da23d7e6dd3d07ca0cc93aa460628
zyb7539 <145760586+zyb7539@users.noreply.github.com>
2023-12-23 17:10:52 +0800

    pull commit

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("Hello git4");
         System.out.println("master test");
         System.out.println("push test");
+        System.out.println("pull test");
 
     }
 }
