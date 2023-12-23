741de1ef5614c156a9622c733a2383e6fd11d119
zyb <zyb.com>
2023-12-23 16:11:48 +0800

    second commit

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -3,5 +3,7 @@ package org.example;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("Hello git2");
+
     }
 }
