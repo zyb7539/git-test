3c2ea35dc2108d53b35c7b7a31269e7075ba67a9
zyb <zyb.com>
2023-12-23 16:22:59 +0800

    master commit2

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -6,5 +6,7 @@ public class GitTest {
         System.out.println("Hello git2");
         System.out.println("Hello git3");
         System.out.println("Hello git4");
+        System.out.println("master test");
+
     }
 }
